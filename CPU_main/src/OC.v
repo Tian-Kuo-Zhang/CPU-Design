99edc0ae7e1dfa71c40ca0d5954dd4db00fa23f5
ZKY9 <zky0325@gmail.com>
2026-03-30 06:04:15 -0700

    Update OC.v

--- CPU_main/src/OC.v
+++ CPU_main/src/OC.v
@@ -1,13 +1,13 @@
 module OC (
-    input  [15:0] Decode_Out,
-    input  [5:0]  Funct,
-    input         Branch_flag,
+    input  [3:0] Opcode,
+    input  [5:0] Funct,
+    input        Branch_flag,
 
     output reg       RegWrite,
     output reg       ALUSrc,
     output reg       MemtoReg,
     output reg [1:0] PCSrc,
-    output reg [1:0] Op_in,
+    output reg [1:0] ALUOp,
     output reg       MemRead,
     output reg       MemWrite
 );
@@ -18,83 +18,82 @@ always @(*) begin
     ALUSrc   = 1'b0;
     MemtoReg = 1'b0;
     PCSrc    = 2'b00;
-    Op_in    = 2'b00;
+    ALUOp    = 2'b00;
     MemRead  = 1'b0;
     MemWrite = 1'b0;
 
     // mov imm   opcode = 0001
-    if (Decode_Out[1]) begin
+    if (Opcode == 4'b0001) begin
         RegWrite = 1'b1;
         ALUSrc   = 1'b1;
-        Op_in    = 2'b00;
+        ALUOp    = 2'b00;
     end
 
     // load      opcode = 0010
-    else if (Decode_Out[2]) begin
+    else if (Opcode == 4'b0010) begin
         RegWrite = 1'b1;
         ALUSrc   = 1'b1;
         MemRead  = 1'b1;
         MemtoReg = 1'b1;
-        Op_in    = 2'b00;
+        ALUOp    = 2'b00;
     end
 
     // store     opcode = 0011
-    else if (Decode_Out[3]) begin
+    else if (Opcode == 4'b0011) begin
         ALUSrc   = 1'b1;
         MemWrite = 1'b1;
-        Op_in    = 2'b00;
+        ALUOp    = 2'b00;
     end
 
     // addi      opcode = 0100
-    else if (Decode_Out[4]) begin
+    else if (Opcode == 4'b0100) begin
         RegWrite = 1'b1;
         ALUSrc   = 1'b1;
-        Op_in    = 2'b00;
+        ALUOp    = 2'b00;
     end
 
     // subi      opcode = 0101
-    else if (Decode_Out[5]) begin
+    else if (Opcode == 4'b0101) begin
         RegWrite = 1'b1;
         ALUSrc   = 1'b1;
-        Op_in    = 2'b01;
+        ALUOp    = 2'b01;
     end
 
     // R-type    opcode = 0000
-    else if (Decode_Out[0]) begin
+    else if (Opcode == 4'b0000) begin
         RegWrite = 1'b1;
         ALUSrc   = 1'b0;
 
         case (Funct)
-            6'b000000: Op_in = 2'b00; // mov reg
-            6'b000001: Op_in = 2'b00; // add reg
-            6'b000010: Op_in = 2'b01; // sub reg
-            6'b000011: Op_in = 2'b10; // and reg
-            6'b000100: Op_in = 2'b11; // or reg
+            6'b000000: ALUOp = 2'b00; // mov reg
+            6'b000001: ALUOp = 2'b00; // add reg
+            6'b000010: ALUOp = 2'b01; // sub reg
+            6'b000011: ALUOp = 2'b10; // and reg
+            6'b000100: ALUOp = 2'b11; // or reg
             default: begin
                 RegWrite = 1'b0;
-                Op_in    = 2'b00;
+                ALUOp    = 2'b00;
             end
         endcase
     end
 
     // jump      opcode = 0110
-    else if (Decode_Out[6]) begin
+    else if (Opcode == 4'b0110) begin
         PCSrc = 2'b10;
-        // ALUSrc / Op_in don't care here, keep default
     end
 
     // beq       opcode = 0111
-    else if (Decode_Out[7]) begin
+    else if (Opcode == 4'b0111) begin
         ALUSrc = 1'b0;
-        Op_in  = 2'b01;   // compare by subtraction
+        ALUOp  = 2'b01;
         if (Branch_flag)
             PCSrc = 2'b01;
     end
 
     // bne       opcode = 1000
-    else if (Decode_Out[8]) begin
+    else if (Opcode == 4'b1000) begin
         ALUSrc = 1'b0;
-        Op_in  = 2'b01;   // compare by subtraction
+        ALUOp  = 2'b01;
         if (!Branch_flag)
             PCSrc = 2'b01;
     end
